362a9ac84f9db09a75d9396fd456531e4fc8e8e5
Muhammad Rahman <mrmm2703@gmail.com>
2020-12-16 09:24:21 +0000

    Finished login procedure

--- dashboard.php
+++ dashboard.php
@@ -3,3 +3,16 @@ echo "It worked!!!<br><br>Session variables:<br>";
 session_start();
 var_dump($_SESSION);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Together! - Dashboard</title>
+</head>
+<body>
+    <div class="header">
+        
+    </div>
+</body>
+</html>
